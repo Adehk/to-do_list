759f041e11815898c3f0934f32dd7a3039f5176c
Adehk <khedagermikhanova@mail.ru>
2023-10-30 13:29:15 +0300

    .

--- src/select/Select.jsx
+++ src/select/Select.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-
 const Select = ({ setTasks }) => {
-  const [select, setselect] = useState("All");
-  return (
-    <select
-      className="todo__options"
-      onClick={(e) => setselect(e.target.value)}
-    >
-      <option value="all">Все</option>
-      <option value="active">Активные</option>
-      <option value="completed">Завершённые</option>
-    </select>
-  );
+  <select className="todo__options">
+    <option value="all">Все</option>
+    <option value="active">Активные</option>
+    <option value="completed">Завершённые</option>
+  </select>;
 };
 
 export default Select;
